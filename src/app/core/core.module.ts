16b70b0febd24abaa7cb2951a5788e83d61bdfc3
A-STAR <andreysuperstar@gmail.com>
2018-04-29 00:57:48 +0300

    chore(core): remove CommonModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,8 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    CommonModule
-  ],
-  exports: [
-    BrowserAnimationsModule
-  ]
+  imports: [BrowserAnimationsModule],
+  exports: [BrowserAnimationsModule]
 })
 export class CoreModule { }
